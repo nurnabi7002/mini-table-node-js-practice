944b68e625b07c669e45f64b92ca7b3a0ab2725f
MD Nur Nabi Khan Rabib <nurnabi7002@gmail.com>
2025-07-28 08:49:23 +0600

    Add files via upload

---
+++ table.js
@@ -0,0 +1,28 @@
+// Make sure you have installed Node JS
+// For Run This code - open terminal and paste - node table.js
+
+let num = 9; // Input your any number or if using prompt, then connect this .js file to a html file and use live server
+
+for (let i = 1; i <= 10; i++) {
+    // Declare i as a 1 for start, then logic is when i is less than 10 then will be i++ as long as the logic is complete.
+
+    console.log(`${i} x ${num} = ${i*num}`);
+    
+    // First logging to console using  template literal, fisrt showing i, num and "=" as a text or string for visual indicate, then we used I multiplay i and num, i goes to 1 to 10 and this is what needs for table, then multipaly all with input or 9 (as an input)
+    
+}
+
+
+
+// Fresh code
+
+/*
+
+
+let num = 9;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i} x ${num} = ${i*num}`);   
+}
+
+
+*/
